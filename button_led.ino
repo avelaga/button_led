62cc124a1118599aa4d68be33c85f6ec564a2851
Abhi Velaga <abhinav.velaga@gmail.com>
2020-01-17 14:44:37 -0600

    added creep + rainbow boot

--- button_led.ino
+++ button_led.ino
@@ -7,6 +7,8 @@ int brightness = 0;
 float hue = 0;
 float inc = 0;
 float hueInc = .3;
+float incSpeed;
+
 int mode = 0;
 int recent = 0;
 int last = 0;
@@ -21,18 +23,120 @@ void setup() {
   pinMode(buttonPin, INPUT);
   LEDS.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
   LEDS.setBrightness(255);
-  bootUp();
+  incSpeed = random(4, 6) / 100.0;
+   //  bootUp();
+  creepBootUp();
+  holdWhite();
+  toColor();
+  //  hue =
+  Serial.begin(9600);
 }
 
 void bootUp() {
 
-  while (brightness < 254) {
+  while (brightness < 255) {
     LEDS.setBrightness(brightness);
-    loop();
+    //    loop();
+    for (int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CRGB( 255, 255, 255);
+      //      hue += .5; // incremenration of hues in the strip
+    }
+
+    //    leds[i] = CRGB( 50, 100, 150);
+
+    FastLED.show();
+
     brightness += 1;
     delay(5);
   }
+  LEDS.setBrightness(255);
 
+}
+
+void creepBootUp() {
+  float start = 0;
+  float bright = 0;
+  
+  while (start < NUM_LEDS+10) {
+    start+=.5;
+    for (int i = 0; i < NUM_LEDS; i++) {
+      if(i<=start){
+      bright = (start - i)*3;
+      if(bright>255){
+        bright=255;
+      }
+      
+      leds[i] = CRGB( bright, bright, bright);
+      }
+      else{
+        leds[i] = CRGB( 0, 0, 0);
+      }
+    }
+    FastLED.show();
+    delay(5);
+  }
+
+}
+
+void holdWhite() {
+  float start = 0;
+  float localHue = start;
+  start += .7;
+  for (int x = 0; x < 60; x++) { // run for 400 frames
+    for (int i = 0; i < NUM_LEDS; i++) {
+
+      // for color strobe
+      //      if (x % 3 == 0) {
+      //        leds[i] = CRGB( 255, 0, 0);
+      //      }
+      //      else if(x%3==1){
+      //        leds[i] = CRGB( 0, 255, 0);
+      //      }
+      //      else {
+      //        leds[i] = CRGB( 0, 0, 255);
+      //      }
+
+      // small moving colors - pretty!
+
+      if(x==59){
+        leds[i] = CRGB( 255, 255, 255);
+      }else{
+            leds[i] = CHSV(localHue, 255, 255);
+            localHue += 65;
+      }
+
+      // strobe
+//      if (x % 2 == 0) {
+//        leds[i] = CRGB( 255, 255, 255);
+//      } else {
+//        leds[i] = CRGB( 0, 0, 0);
+//      }
+
+//    leds[i] = CRGB( 255, 255, 255);
+
+      
+    }
+    FastLED.show();
+    delay(50);
+  }
+  
+}
+
+
+
+void toColor() {
+  float g = 255;
+  float b = 255;
+  float changeSpeed = .6;
+
+  while (g > 0 ) {
+    g -= changeSpeed;
+    b -= changeSpeed;
+    for (int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CRGB( 255, g, b);
+    }
+    FastLED.show();
+  }
 }
 
 void checkButton() {
